15a84467c67d065c915c2210138ac75c6dc172dd
Leejeuk213 <73926434+Leejeuk213@users.noreply.github.com>
2022-08-22 22:55:40 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+printf("         ,r'\"7\n");
+printf("r`-_   ,'  ,/\n");
+printf(" \\. \". L_r'\n");
+printf("   `~\\/\n");
+printf("      |\n");
+printf("      |\n");
+return 0;
+}
