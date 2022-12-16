549df68bcf46de8d61e6ecd97e7f9dc38c0f1dcd
Leejeuk213 <73926434+Leejeuk213@users.noreply.github.com>
2022-12-16 19:00:56 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string.h>
+using namespace std;
+int n,m;
+int temp=0;
+int check[10001];
+vector<int> ans;
+vector<int> v;
+void dfs(int depth)
+{
+    if(depth==m)
+    {
+        for(int i=0;i<ans.size();i++)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }   
+    for(int i=0;i<n;i++)
+    {
+        if(ans[ans.size()-1]<=v[i])
+        {
+            ans.push_back(v[i]);
+            dfs(depth+1);
+            ans.pop_back();
+        }
+        while(v[i+1]==v[i]&&i+1<n)
+        {
+            i++;
+        }
+    }
+}
+int main()
+{
+    cin >>n>>m;
+    for(int i=0;i<n;i++)
+    {
+        int num;
+        cin>>num;
+        v.push_back(num);
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++)
+    {
+        ans.clear();
+        ans.push_back(v[i]);
+        dfs(1);
+        while(v[i+1]==v[i]&&i+1<n)
+        {
+            i++;
+        }
+    }
+    return 0;
+}
